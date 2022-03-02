713937c2945c8e8f0aa0dfa2440f6d47301b1201
Bigbadwolf21 <66219396+Bigbadwolf21@users.noreply.github.com>
2022-03-02 23:28:13 +0100

    Add files via upload

---
+++ AgeInDogYears.js
@@ -0,0 +1,20 @@
+// declare variable my age
+const myAge = 23;
+// assign 2 to the variable early Years
+let earlyYears = 2;
+// reassign a new value to earlyYears
+earlyYears *= 10.5;
+// declare a new variable and assign myAge - 2
+let laterYears = myAge - 2;
+// multiply laterYear by 4
+laterYears *= 4;
+// print earlyYears to the console.
+console.log(earlyYears);
+// print laterYears to the console
+console.log(laterYears);
+// add earlyYears and laterYears to get my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+// convert my name to lowercase using the lowercase method, and save in a variable called myNamwe
+let myName = 'Ayodele Ayomide'.toLowerCase();
+// use string interpolation to print to the console.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
